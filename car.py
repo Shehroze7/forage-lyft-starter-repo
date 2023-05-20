6d7b164829717c31ae9468420d14f4770f467ee9
lovejews <shehrozeshahzad2@gmail.com>
2023-05-20 23:21:26 +0500

    added code in three folders

--- car.py
+++ car.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+    def __init__(self, Battery, engine):
+        self.Battery = Battery
+        self.engine = engine
 
     @abstractmethod
     def needs_service(self):
-        pass
+        return self.engine.needs_service() or self.Battery.needs_service()
